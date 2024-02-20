ed6e5922d259a6e1473f77be0f8ee6a968de61d6
wandersonwendel <wandsolopes@alu.ufc.br>
2024-02-20 10:04:54 -0300

    feat: Implementando Controller para lidar com a lógica de regras de negócio - 'drivers/', para tratar bibliotecas externas em um lugar mais concentrado, - Cria o arquivo 'barcode_handler.py', que implementa um gerenciador para usar a biblioteca barcode e sua manutenção, caso seja preciso, - cria o arquivo 'tag_creator_controller.py', que implementa as responsabilidades de implementar as regras de negócio, - Chama o Controller de tag, na View, para fazer a comunicação entre ambos.

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,17 +1,19 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
         Responsável por interagir com o Http
     '''
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # Lógica de regra de negócio
-        print("Estou na minha view")
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
 
         # Retorno HTTP
-        return HttpResponse(status_code = 200, body = { "hello": "wolrd"})
+        return HttpResponse(status_code = 200, body = formatted_response)
